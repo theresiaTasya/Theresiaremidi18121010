1e4f04076d3b96ef6623ab2c7325c44e63b3d137
tsya <welerubuntsya@gmail.com>
2024-07-15 21:26:17 +0700

    first init github

---
+++ database/seeders/ProfileSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('misis')->insert([
+            'Image' => '20211219021353.png',
+        ]);
+    }
+}
